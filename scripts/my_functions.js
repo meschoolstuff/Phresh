7009f83e5d880eb35e6187316573f3500fa6d41c
Mike Lim <mslim96@gmail.com>
2021-03-26 16:20:05 -0700

    group-code is retrieved from db

--- scripts/my_functions.js
+++ scripts/my_functions.js
@@ -16,3 +16,21 @@ function sayHello(){
 }
 sayHello();
 
+function sayGroupCode() {
+    firebase.auth().onAuthStateChanged(function(somebody){
+        if(somebody){
+            console.log(somebody.uid);
+            db.collection("users")
+            .doc(somebody.uid)
+            .get()                      // Read!
+            .then(function(doc){
+                // console.log(doc.data().name);
+                var n = doc.data().groupCode;
+                $("#code-goes-here").text(n);
+                // do other things and get other things
+            })
+        }
+    })
+}
+sayGroupCode();
+
